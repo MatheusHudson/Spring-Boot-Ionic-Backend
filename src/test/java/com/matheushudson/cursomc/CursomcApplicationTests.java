ea6d12c0b7a6b38af18631cd3c3cc2f332825fd0
Matheus Hudson de Souza <matheushudson2013@gmail.com>
2020-12-24 15:20:52 -0300

    Projeto criado

---
+++ src/test/java/com/matheushudson/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheushudson.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
