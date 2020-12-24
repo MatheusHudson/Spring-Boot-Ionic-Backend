b49c3974f86a85008cd0f1c0a2c90467d4b243bb
Matheus Hudson de Souza <matheushudson2013@gmail.com>
2020-12-24 18:43:10 -0300

    Ajustes no endpoint Categoria
    Tramaneto de exceção da nossa requisição.

--- src/main/java/com/matheushudson/cursomc/services/CategoriaService.java
+++ src/main/java/com/matheushudson/cursomc/services/CategoriaService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.matheushudson.cursomc.domain.Categoria;
 import com.matheushudson.cursomc.repositories.CategoriaRepository;
+import com.matheushudson.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
